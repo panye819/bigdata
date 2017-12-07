18f65ba3b32aa67176c2b59ccdce452d066b0780
panye819 <panye819@gmail.com>
2017-12-07 13:57:43 +0800

    touch HelloSpark

---
+++ HelloSpark/src/main/scala/cn/itcast/spark/day5/StreamingWordCount.scala
@@ -0,0 +1,29 @@
+package cn.itcast.spark.day5
+
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.{SparkConf, SparkContext}
+
+object StreamingWordCount {
+  def main(args: Array[String]): Unit = {
+
+    //设置日志级别，调整到WARN
+    LoggerLevels.setStreamingLogLevels()
+    //设置配置
+    val conf = new SparkConf().setAppName("StreamingWordCount").setMaster("local[2]")
+    val sc = new SparkContext(conf)
+    val ssc = new StreamingContext(sc,Seconds(15))
+
+    //接收数据
+    val ds = ssc.socketTextStream("127.0.0.1",8888)
+    //Dstream是一个特殊的RDD
+    val result = ds.flatMap(_.split(" ")).map((_,1)).reduceByKey(_+_)
+    //打印结果
+    result.print()
+    //启动进程
+    ssc.start()
+    //进程等待关闭
+    ssc.awaitTermination()
+
+  }
+
+}
